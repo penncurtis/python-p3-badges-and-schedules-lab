1974e2d1d97ea00a91ebb257fe372eba81a4a6df
Penn Curtis <penn.s.m.c@gmail.com>
2023-05-23 14:19:09 -0400

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms = range(1,9)
+    assign = []
+    for room in rooms:
+        assign.append(f"Hello, {names[room - 1]}! You\'ll be assigned to room {room}!")
+    return assign
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+
+    for assign in assign_rooms(names):
+        print(assign)
